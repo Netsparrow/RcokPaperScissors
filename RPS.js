449765f0a834221ce9375b6b0dfbcc82a34301a1
Byron Reyes <47467726+N3tsparrow@users.noreply.github.com>
2023-07-03 02:16:01 -0700

    I finally stopped over thinking this mini project and finally finished it

--- RPS.js
+++ RPS.js
@@ -1,62 +1,67 @@
-function getComputerChoice(){
-    random = Math.floor(Math.random() * (4 - 1)) + 1;
-    switch(random){
-        case 1:
-            return "rock";
-        break
-        case 2:
-            return "paper";
-        break
-        case 3:
-            return "scissors";
+let playerChoice = choice => {
+    choice = choice.toLowerCase();
+    if (choice =='rock' || choice =='paper' || choice =='scissors'){
+        return choice
+    }else{
+        console.log('Please choose either rock, paper, or scissors. Or check your spelling')
     }
-
-};
-
-function playRound(){
-    switch(playerSelection, computerSelection){
-        case "rock" && "paper":
-            return "The player chose " + playerSelection + " and the computer chose " + computerSelection + ". Therfore, the COMPUTER WINS!" ;
-        break 
-        case "rock" && "scissors":
-            return "The player chose " + playerSelection + " and the computer chose " + computerSelection + ". Therfore, the PLAYER WINS! "
-        break
-        case "rock" && "rock":
-            return "Both the player and computer chose rock, there THIS IS A DRAW!"
-        break
-        case "paper" && "scissors":
-            return "The player chose " + playerSelection + " and the computer chose " + computerSelection + ". Therfore, the COMPUTER WINS!"
-        break
-        case "paper" && "rock":
-            return "The player chose " + playerSelection + " and the computer chose " + computerSelection + ". Therfore, the PLAYER WINS!"
-        break
-        case "paper" && "paper":
-            return "Both the player and computer chose paper, there THIS IS A DRAW!"
-        break
-        case "scissors" && "rock":
-            return "The player chose " + playerSelection + " and the computer chose " + computerSelection + ". Therfore, the COMPUTER WINS!"
-        break
-        case "scissors" && "papers":
-            return "The player chose " + playerSelection + " and the computer chose " + computerSelection + ". Therfore, the PLAYER WINS!"
-        break
-        case "scissors" && "scissors":
-            return "Both the player and computer chose paper, there THIS IS A DRAW!"
-    };
-         
-
-
-
-
 }
- 
-let playerSelection = "paper";
-let computerSelection = getComputerChoice();
 
 
-console.log("player: "+ playerSelection);
-console.log("computer: "+ computerSelection);
+let compChoice = () =>{
+    const random = Math.floor(Math.random() * (4 - 1)) + 1; 
 
-console.log(playRound());
+    switch (random){
+        case 1:
+            return 'rock';
+            break;
+        case 2:
+            return 'paper';
+            break;
+        case 3:
+            return 'scissors';
+            break;
+    }
+} 
+
+
+let computer = compChoice();
+let player = playerChoice('scissors');
 
 
 
+
+let game = () =>{ 
+    if (computer === 'rock'){
+        if (player === 'rock'){
+            console.log('Both the computer and yourself chose rock. therefore it is a draw.')
+        }else if (player === 'paper'){
+            console.log('The computer chose rock and you chose paper. Therefore you win!')
+        }else if (player === 'scissors'){
+            console.log('The computer chose rock and you chose scissors. Therefore the computer wins!')
+        }
+    }else if ( computer === 'paper'){
+        if (player === 'rock'){
+            console.log('The computer chose paper and you chose rock, therefore the computer wins!')
+        }else if (player === 'paper'){
+            console.log('Both the commputer and youself chose paper, therefore it is a draw.')
+        }else if (player === 'scissors'){
+            console.log('The computer chose paper and you chose scissors, therefore you win!')
+        }
+
+    }else if (computer === 'scissors'){
+        if (player === 'rock'){
+            console.log('The computer chose scissors and you chose rock, therefore you win!')
+        }else if (player === 'paper'){
+            console.log('The computer chose scissors and you chose paper, therefore the computer wins.')
+        }else if (player === 'scissors'){
+            console.log(' Both the commputer and youself chose scissors, therefore it is a draw.')
+        }
+    }
+}
+
+console.log(`Computer Choice: ${computer}`);
+console.log(`Player Choice: ${player}`);
+
+
+game();
