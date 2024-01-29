e3e567c6ae50e092956ef276a7d8daa98fcc28e3
Byron Reyes <byronruben110@gmail.com>
2024-01-29 05:11:23 -0700

    Refactored code and started using arrow function and ternanry operators. Next I want to add a prompt input and to see if I can use ternary ops for the Else Ifs to condense code down.

--- RPS.js
+++ RPS.js
@@ -1,67 +1,69 @@
-let playerChoice = choice => {
-    choice = choice.toLowerCase();
-    if (choice =='rock' || choice =='paper' || choice =='scissors'){
-        return choice
-    }else{
-        console.log('Please choose either rock, paper, or scissors. Or check your spelling')
+//if = if statement 
+//eo = end of
+
+let compPick = function(){
+    let rando = Math.floor(Math.random() * 3);
+     return (rando === 0) ? 'rock': 
+         (rando === 1) ? 'paper': 'scissors'
+}
+
+game = (userinput) => {
+
+  let comp = compPick()
+  console.log(comp)
+
+  if (userinput === 'rock'){ //if#1
+    if (comp === 'rock'){
+      return `IT'S A TIE! You both chose rock`;
+    }else if (comp === 'paper'){ 
+      return 'COMPUTER WINS! Computer chose paper. You chose rock.'
+  } else{
+    return 'YOU WIN! Computer chose scissors. You chose rock.'};//eo if#1
+  }else if (userinput === 'paper'){ //if#2
+    if (comp === 'rock'){
+      return 'YOU WIN! Computer chose rock. You chose paper.'
+    }else if (compChoice === 'paper'){
+      return `IT's a TIE! You both chose paper.`
+    }else {return 'COMPUTER WINS! Computer chose scissors. You chose paper.'};//eo if#2
+  }else{
+    if (userinput === 'scissors'){ //if#3
+      if (comp === 'rock'){
+        return 'COMPUTER WINS! Computer chose rock. You chose scissors.'
+      }else if (comp === 'paper'){
+        return 'YOU WIN! Computer chose paper. You chose scissors'
+      }else{ return `IT's A TIE! You both chose scissors`} //EO IF#3
     }
+  }
 }
 
 
-let compChoice = () =>{
-    const random = Math.floor(Math.random() * (4 - 1)) + 1; 
-
-    switch (random){
-        case 1:
-            return 'rock';
-            break;
-        case 2:
-            return 'paper';
-            break;
-        case 3:
-            return 'scissors';
-            break;
-    }
-} 
-
-
-let computer = compChoice();
-let player = playerChoice('scissors');
+console.log(game('rock'))
 
 
 
 
-let game = () =>{ 
-    if (computer === 'rock'){
-        if (player === 'rock'){
-            console.log('Both the computer and yourself chose rock. therefore it is a draw.')
-        }else if (player === 'paper'){
-            console.log('The computer chose rock and you chose paper. Therefore you win!')
-        }else if (player === 'scissors'){
-            console.log('The computer chose rock and you chose scissors. Therefore the computer wins!')
-        }
-    }else if ( computer === 'paper'){
-        if (player === 'rock'){
-            console.log('The computer chose paper and you chose rock, therefore the computer wins!')
-        }else if (player === 'paper'){
-            console.log('Both the commputer and youself chose paper, therefore it is a draw.')
-        }else if (player === 'scissors'){
-            console.log('The computer chose paper and you chose scissors, therefore you win!')
-        }
 
-    }else if (computer === 'scissors'){
-        if (player === 'rock'){
-            console.log('The computer chose scissors and you chose rock, therefore you win!')
-        }else if (player === 'paper'){
-            console.log('The computer chose scissors and you chose paper, therefore the computer wins.')
-        }else if (player === 'scissors'){
-            console.log(' Both the commputer and youself chose scissors, therefore it is a draw.')
-        }
-    }
-}
-
-console.log(`Computer Choice: ${computer}`);
-console.log(`Player Choice: ${player}`);
-
-
-game();
+// 
+// ### UPDATED - I NOW KNOW THAT THIS BOTTOM CODE WOULDNT EVEN WORK IF TERNARY OPS WORKED. I NEED TO CALL THE FUNCTION FIRST BY ASSIGNING IT TO A VARIABLE. THEN SEE IF THE TERNARY OPS DO WORK?
+// let compChoice = () => {
+//   let rando = Math.floor(Math.random() * 1);
+//    return (rando === 0) ? 'rock': 
+//        (rando === 1) ? 'paper': 'scissors'
+//  }  
+//  console.log(compChoice())
+ 
+//  Game = (userInput) =>{
+//    return /*1I*/(userInput === 'rock') ? /*1.1I*/(compChoice === 'rock') ? `It's a tie, you both chose rock` /*1.2EI*/: 
+//                       (compChoice === 'paper') ? 'Computer wins! Computer chose Rock whcihj beats paper' : 
+//                       (compChoice === 'scissors') ? 'YOU WIN! computer chose scissors and Rock beats scissors!' /*1E*/:'test'
+//    //(userInput === 'paper') ? :
+//    //(compChoice === 'rock') ? 
+ 
+//    // return /*1I*/(userInput === 'rock') ? /*1.1I*/(compChoice === 'rock') ? `It's a tie, you both chose rock` /*1.2EI*/: (compChoice === 'paper') ? 'Computer wins! Computer chose Rock whcihj beats paper' : (compChoice === 'scissors') ? 'YOU WIN! computer chose scissors and Rock beats scissors!' /*1E*/: 
+//    // 'test'
+//    // //(userInput === 'paper') ? :
+//    // //(compChoice === 'rock') ? 
+//  :'test'
+//  }
+ 
+//  Game('rock')
